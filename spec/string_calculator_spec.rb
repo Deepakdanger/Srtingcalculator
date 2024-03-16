37696ce53bfb22b205ae0a319dcbe2daa5b8b84d
Deepakdanger <depk.96.kumar@gmail.com>
2024-03-16 21:58:25 +0530

    Test for adding number for string contain two number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -11,5 +11,11 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add_method("32")).to eq(32)
       expect(StringCalculator.add_method("334")).to eq(334)
     end
+
+    it "return the sum of number if string has two numbers" do
+      expect(StringCalculator.add_method("1, 2")).to eq(3)
+      expect(StringCalculator.add_method("32, 33")).to eq(65)
+      expect(StringCalculator.add_method("334, 334")).to eq(668)
+    end
  end
 end
