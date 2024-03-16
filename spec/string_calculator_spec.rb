75ced233fc4f8f3e8911216cea9f9593efae114e
Deepakdanger <depk.96.kumar@gmail.com>
2024-03-16 20:59:52 +0530

    Added the method "add_method" & return 0 for empty string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,7 +3,7 @@ require_relative '../string_calculator'
 RSpec.describe StringCalculator do
   describe "add_method" do
     it "return 0 if string is empty" do
-      except(StringCalculator.add_method("")).to eq(0)
+      expect(StringCalculator.add_method("")).to eq(0)
     end
  end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,3 +1,5 @@
 class StringCalculator
-
+  def self.add_method(string_numbers)
+    return 0 if string_numbers.empty?
+  end
 end
