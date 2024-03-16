31f296eaf22232179b0e31f46ff70223689e0e42
Deepakdanger <depk.96.kumar@gmail.com>
2024-03-16 21:05:31 +0530

    Test for the single number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,11 @@ RSpec.describe StringCalculator do
     it "return 0 if string is empty" do
       expect(StringCalculator.add_method("")).to eq(0)
     end
+
+    it "return the number if string has single number" do
+      expect(StringCalculator.add_method("1")).to eq(1)
+      expect(StringCalculator.add_method("32")).to eq(32)
+      expect(StringCalculator.add_method("334")).to eq(334)
+    end
  end
 end
