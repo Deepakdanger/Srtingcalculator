7a54ad1a2a38c07bb5b89d85b160d07f22f04a2f
Deepakdanger <depk.96.kumar@gmail.com>
2024-03-17 01:01:23 +0530

    Code to recognise the different delimiters present in string  do operation

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,12 @@
 class StringCalculator
   def self.add_method(string_numbers)
     return 0 if string_numbers.empty?
-    string_numbers.split(/,|\n/).map(&:to_i).sum
+    del = ","
+    if string_numbers.start_with?("//")
+      del = string_numbers[2]
+      string_numbers = string_numbers.split("\n",2)[1]
+    end
+
+    string_numbers.split(/,|\n|#{del}/).map(&:to_i).sum
   end
 end
